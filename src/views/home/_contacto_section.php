8a3a3e1e7771837eff111575bf98104ae2885d19
Juan Antonio Torres <95553786+jatoni@users.noreply.github.com>
2023-03-01 15:34:41 -0600

    Se hace la seccion de contactos, faltan algunos detalles al igual que el responsive

--- src/views/home/_contacto_section.php
+++ src/views/home/_contacto_section.php
@@ -1,1 +1,24 @@
-<h1>Contacto</h1>
+<section class="container-contacto">
+    <div class="titulo-seccion-contacto">
+        <h1>Platicanos tu d&iacute;a</h1>
+        <div class="form-contacto">
+            <form method="get" action="">
+                <div>
+                    Email*:<br/>
+                    <input name="email" type="email" placeholder="Escribe tu email aqui" required />
+                </div>
+                <div>
+                    Telefono*:<br/>
+                    <input name="telefono" type="text" placeholder="Escribetu telefono aqui" required />
+                </div>
+                <div>
+                    Escribemos tu idea:<br/>
+                    <textarea name="ideas" cols="30" rows="10" style="resize: none;" placeholder="Escribe tu idea aqui"></textarea>
+                </div>
+                <div style="text-align: center;">
+                    <button>Enviar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
--- src/views/home/index.php
+++ src/views/home/index.php
@@ -1,5 +1,2 @@
 <?php
     include '_contacto_section.php';
-    include '_portafolio_section.php';
-    include '_reeldemo_section.php';
-?>
